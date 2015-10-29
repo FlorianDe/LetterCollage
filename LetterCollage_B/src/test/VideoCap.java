e23eaa2eb739ee78ba6d6b495ae1a2f00b83aabb
FlorianDe <florian.decker@uni-oldenburg.de>
2015-10-29 03:37:07 +0100

    Implemented basic face&eye detection, atm with hardcoded pathes to the haarcascade files!

--- LetterCollage_B/src/test/VideoCap.java
+++ LetterCollage_B/src/test/VideoCap.java
@@ -14,7 +14,14 @@ import javax.swing.JFrame;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.features2d.FeatureDetector;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.videoio.VideoCapture;
 /*
 import org.opencv.highgui.Highgui;        
@@ -22,53 +29,68 @@ import org.opencv.highgui.VideoCapture;
   */      
 public class VideoCap extends JFrame{
 	
-    public static void main (String args[]){
-    	/*
+	private static final String haarcascades_frontalface = "C:\\Users\\Florian\\Documents\\Git\\LetterCollage_B\\LetterCollage_B\\res\\haarcascades\\haarcascade_frontalface_default.xml";
+	private static final String haarcascades_eye = "C:\\Users\\Florian\\Documents\\Git\\LetterCollage_B\\LetterCollage_B\\res\\haarcascades\\haarcascade_eye.xml";
+
+	
+	private static final long serialVersionUID = -1421396949037115883L;
+
+	public static void main (String args[]){
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     	VideoCap vc = new VideoCap();
+    	/*
     	JFrame mainWindow = vc.initializeUI();
     	vc.openCVVidoReader(mainWindow);
     	*/
 
 		MainFrame mf = new MainFrame();
-		openCVVidoReader(mf);
+		vc.openCVVidoReader(mf);
     }
     
-    public static void openCVVidoReader(MainFrame mainWindow){
-    	System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    public void openCVVidoReader(MainFrame mainWindow){
+    	//System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     	VideoCapture camera = new VideoCapture(0);
 		MatOfByte mof = new MatOfByte();
 		String path = System.getProperty("user.home") + "/Desktop" + "/camera.jpg";
-		Mat frame = null;
+		Mat image = null;
 		BufferedImage bufImage = null;
 		
     	if(!camera.isOpened()){
     		System.out.println("Error");
     	}
     	else {
-    		frame = new Mat();
+    		image = new Mat();
     	    while(true){
-    	    	if (camera.read(frame)){
-	    	    	if (!frame.empty()){
-	    	    		System.out.println("Captured Frame Width " + frame.width() + " Height " + frame.height());
+    	    	if (camera.read(image)){
+	    	    	if (!image.empty()){
+	    	    		System.out.println("Captured Frame Width " + image.width() + " Height " + image.height());
 	    	    		
-	    	    		/*
-	    	    		//////
-	    	    		Imgcodecs.imencode(path, frame, mof);
-	    	    		byte[] byteArray = mof.toArray(); // Mapping #2
-
-	    	    		InputStream in = new ByteArrayInputStream(byteArray);
-	    	    		try {
-	    	    			bufImage = ImageIO.read(in); // Mapping #3
-	    	    		} catch (IOException e) {
-	    	    			e.printStackTrace();
+	    	    		// Detect faces in the image.
+	    	    	    // MatOfRect is a special container class for Rect.
+	    	    		//CascadeClassifier faceDetector = new CascadeClassifier(this.getClass().getClassLoader().getResource("haarcascades/haarcascade_frontalface_default.xml").getPath());
+	    	    		//Kleiner fail, CascadeClassifier WILL ein pfad, aber classLoader kann keinen internen Pfad zurückgeben, somit nun erstmal hardcoded....
+	    	    		CascadeClassifier faceDetector = new CascadeClassifier(haarcascades_frontalface);
+	    	    		CascadeClassifier eyeDetector = new CascadeClassifier(haarcascades_eye);
+	    	    		if (!faceDetector.empty()) {
+		    	    		MatOfRect faceDetections = new MatOfRect();
+		    	    		MatOfRect eyeDetections = new MatOfRect();
+		    	    	    faceDetector.detectMultiScale(image, faceDetections);
+		    	    	    eyeDetector.detectMultiScale(image, eyeDetections);
+		    	    	    System.out.println(String.format("Detected %s faces and %s eyes!", faceDetections.toArray().length, eyeDetections.toArray().length));
+		    	    	    // Draw a bounding box around each face.
+		    	    	    for (Rect rect : faceDetections.toArray()) {
+		    	    	        Imgproc.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0));
+		    	    	    }
+		    	    	    for (Rect rect : eyeDetections.toArray()) {
+		    	    	        Imgproc.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0));
+		    	    	    }
 	    	    		}
-	    	    		
-	    	    		camera.retrieve(frame);
-	    	    		mainWindow.getBuffView().setImage(bufImage);
-	    	    		//////
-	    	    		*/
-	    	    		mainWindow.getBuffView().setImage(matToBufferdImage(frame));
-	    	    		Imgcodecs.imwrite(path, frame);
+	    	    		else{
+	    	    			System.out.println("Facedetector is empty...cannot detect faces!");
+	    	    		}
+
+	    	    		mainWindow.getBuffView().setImage(matToBufferdImage(image));
+	    	    		Imgcodecs.imwrite(path, image);
 	    	    		//Imgcodecs.imwrite(path, mof);
 	    	    		//Highgui.imwrite("camera.jpg", frame);
 	    	    		//mof.copyTo(frame);
@@ -84,7 +106,7 @@ public class VideoCap extends JFrame{
 		//mainWindow.repaint();
     }
     
-    public static BufferedImage matToBufferdImage(Mat m){
+    public BufferedImage matToBufferdImage(Mat m){
     	int type = BufferedImage.TYPE_BYTE_GRAY;
 
         if ( m.channels() > 1 ) {
@@ -100,4 +122,22 @@ public class VideoCap extends JFrame{
 
         return image;
     }  
+    
+	
+	/*
+	//////
+	Imgcodecs.imencode(path, frame, mof);
+	byte[] byteArray = mof.toArray(); // Mapping #2
+
+	InputStream in = new ByteArrayInputStream(byteArray);
+	try {
+		bufImage = ImageIO.read(in); // Mapping #3
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+	
+	camera.retrieve(frame);
+	mainWindow.getBuffView().setImage(bufImage);
+	//////
+	*/
 }   
